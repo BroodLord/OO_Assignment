1622daf3160569a42c4cb6d55530ba7f87b90447
BroodLord <47176624+BroodLord@users.noreply.github.com>
2019-11-30 20:28:39 +0000

    SAFE MEMORY

--- OO_Assignment/OO_Assignment/MapArrayClass.h
+++ OO_Assignment/OO_Assignment/MapArrayClass.h
@@ -3,6 +3,7 @@
 #include <ostream>
 #include <iostream>
 
+
 /* This is a template class for the interface of the Map Class */
 template<typename TypeKey, typename TypeValue>
 class MapArray
@@ -11,7 +12,7 @@ class MapArray
 private:
 	/* This Template Class is used to store a key and value. */
 	template<typename TypeKey, typename TypeValue>
-	struct KeyValuePair
+	class KeyValuePair
 	{
 	public:
 		TypeKey Key; // Key for the Map, Can be any type.
@@ -25,11 +26,11 @@ private:
 	public:
 		MyIterator() {} // Default constructor
 		MyIterator(KeyValuePair<TypeKey, TypeValue>* Ptr) : MyIteratorPtr(Ptr) {}
-		KeyValuePair<TypeKey, TypeValue>* operator->() {return MyIteratorPtr;} // Reutns either of the Key or Value
+		KeyValuePair<TypeKey, TypeValue>* operator->() { return MyIteratorPtr; } // Reutns either of the Key or Value
 		/*Override function to see if the iterator is equal to a ptr*/
-		bool operator ==(const MyIterator<TypeKey, TypeValue>& KVP) { return MyIteratorPtr == KVP.MyIteratorPtr; }
+		bool operator ==(const MyIterator<TypeKey, TypeValue>& KVP) { return (MyIteratorPtr == KVP.MyIteratorPtr); }
 		/*Override function to see if the iterator isn't equal to a ptr*/
-		bool operator !=(const MyIterator<TypeKey, TypeValue>& KVP) { return MyIteratorPtr != KVP.MyIteratorPtr; }
+		bool operator !=(const MyIterator<TypeKey, TypeValue>& KVP) { return !(MyIteratorPtr == KVP.MyIteratorPtr); }
 		/* These override function will increment and decrement where to point to in the map */
 		MyIterator<TypeKey, TypeValue>& operator ++() { MyIteratorPtr++; return *this; }
 		MyIterator<TypeKey, TypeValue>& operator --() { MyIteratorPtr--; return *this; }
@@ -37,30 +38,236 @@ private:
 		/* Pointer which is used to data and increment where to point in the map*/
 		KeyValuePair<TypeKey, TypeValue>* MyIteratorPtr;
 	};
-
-	const int MAX_SIZE = 2000000; // Total number of elements in the array.
 	const int DEFAULT_SIZE = 20; // Default starting size.
 	const int DYNAMIC_ARRAY_MULTIPLIER = 5; // Number that mulitples the new array size.
+	const std::string MEMORY_MESSAGE = "MEMORY CANNOT BE ALLOCATED ANYMORE";
+	const std::string MAP_CREATED_MESSAGE = "MAP HAS BEEN CREATED";
+	const std::string MAP_DELETED_MESSAGE = "MAP HAS BEEN DELETED";
+	const std::string KEY_NOT_FOUND_MESSAGE = "KEY HASN'T BEEN FOUND";
 	int ArraySize; // Gets track of the ArraySize.
 	int CurrentArraySize = 0; // Keeps track of the amount of elements
-	void AddMemory(); // Adds memory to the array to support dynamic memory allocation.
-	void Sort(); // Sorts the Array.
+	/* This function is used when we need to give the array more memory */
+	void AddMemory()
+	{
+		//int SavedSized;
+		//SavedSized = ArraySize;
+		ArraySize *= DYNAMIC_ARRAY_MULTIPLIER; // This makes the Array Size 5 times better
+		/*TRY CATCH FUNCTION TO TEST MEMEORY ALLOCATION*/
+		try
+		{
+			KeyValuePair<TypeKey, TypeValue>* KVPArray; // Creates a new pointer of KeyValueArray
+			KVPArray = new KeyValuePair<TypeKey, TypeValue>[ArraySize] {}; // Sets up a new Array of the new size
+
+
+		/* Runs through the old Array and assigns all the data to the new Array */
+			for (int i = 0; i < CurrentArraySize; i++)
+			{
+				KVPArray[i].Key = KeyValueArray[i].Key;
+				KVPArray[i].Value = KeyValueArray[i].Value;
+			}
+			delete[] KeyValueArray; // Deletes the old array assign to the pointer
+			KeyValueArray = KVPArray; // makes the old ponter look at the new Array.
+		}
+		catch (std::bad_array_new_length & e)
+		{
+			KeyValueArray = NULL;
+			std::cout << MEMORY_MESSAGE << std::endl;
+		}
+
+	}
+	/* This function is a linear sort to sort all the elements it the array*/
+	void Sort()
+	{
+		if (!IsEmpty())
+		{
+			int min, i, j; // Variables so I can keep track of indexs to change
+			TypeKey tempKey; // Temp variable to sort the Key
+			TypeValue tempValue; // Temp variable to sort the Value
+			/* Runs through all the elements in the array */
+			for (i = 0; i < CurrentArraySize - 1; i++)
+			{
+				min = i; // Sets min to the currenty index that needs sorting
+				for (j = i + 1; j < CurrentArraySize; j++)
+				{
+					/* Check to see if the current index is less then any value in the array */
+					if (KeyValueArray[min].Key < KeyValueArray[j].Key)
+					{
+						min = j; // Sets the min to equal the J
+					}
+				}
+				tempKey = KeyValueArray[i].Key; // Sets the temp key to the current I.
+				tempValue = KeyValueArray[i].Value; // Sets the temp value to the current I.
+				KeyValueArray[i].Key = KeyValueArray[min].Key; // Sets what the index Key will be.
+				KeyValueArray[i].Value = KeyValueArray[min].Value; // Sets what the index Value will be.
+				KeyValueArray[min].Key = tempKey; // Sets the Key in index min to temp
+				KeyValueArray[min].Value = tempValue; // Sets the Value in index min to temp
+			}
+		}
+	}
 	KeyValuePair<TypeKey, TypeValue>* KeyValueArray; // A pointer that is used to point to the Array of KVP.
 
 	/* Public functions avaiable to the Map user */
 public:
 	using Iterator = MyIterator<TypeKey, TypeValue>;
-	MapArray(); // Constructor to set up the Array with a default size.
-	~MapArray(); // Deconstructor to delete the Array.
-	MyIterator <TypeKey, TypeValue> Begin(){ return &KeyValueArray[0]; };
-	MyIterator <TypeKey, TypeValue> End() {	return &KeyValueArray[CurrentArraySize]; };
-	bool IsEmpty(); // Function that checks to see if the Array is empty.
-	void AddKVP(TypeKey Key, TypeValue Value); // This will allow the user to add a key and value to the top of the Map.
-	void RemoveKVP(TypeKey Key); // This will search through the array for and key and remove it from the list.
-	void Clear(); // This will run through each element in the array and reset it.
-	int CurrentSize(); // This will return the amount of elements in the array.
-	bool FindKey(TypeKey Key); // returns if the key already exists.
-	TypeValue GetValue(TypeKey Key); // This will allow the user to search for a value by passing a key,
+	MyIterator <TypeKey, TypeValue> Begin() { return &KeyValueArray[0]; };
+	MyIterator <TypeKey, TypeValue> End() { return &KeyValueArray[CurrentArraySize]; };
+
+	/* This function will set up the Map when it is created */
+	MapArray()
+	{
+		ArraySize = DEFAULT_SIZE; // Gets the size the user entered and assigns it to the ArraySize
+		KeyValueArray = new KeyValuePair<TypeKey, TypeValue>[ArraySize]; // This creates a new array of Keys and values
+		std::cout << MAP_CREATED_MESSAGE << std::endl;
+	}
+	/* This is a deconstructor that is used to delete the Map. */
+	~MapArray()
+	{
+		delete[] KeyValueArray;
+		std::cout << MAP_DELETED_MESSAGE << std::endl;
+	}
+	/* This function will let the user know if the Array is empty */
+	bool IsEmpty()
+	{
+		/* If the Top of the Array is equal or less then 0 then its true, else it false */
+		if (CurrentArraySize <= 0) { return true; }
+		else { return false; }
+	}
+	/* This function will allow data to be add to the map class */
+	void AddKVP(TypeKey Key, TypeValue Value)
+	{
+		bool Added = false; // Boolean to see if the data has already been added
+		/* Runs through all the elements in the array */
+		for (int i = 0; i < CurrentArraySize; i++)
+		{
+			/* This checks to see if the Key already exsists */
+			if (!(KeyValueArray[i].Key < Key) && !(Key < KeyValueArray[i].Key))
+			{
+				/* This will add the new value to the key and set the added bool to true */
+				KeyValueArray[i].Value = Value;
+				Added = true;
+				break;
+			}
+		}
+		/* check to see if the data hasn't been added */
+		if (!Added && KeyValueArray != NULL)
+		{
+			/* This will add a new Key and Value as a new element to the back of the Map
+			   and after adding the data it will sort the data. */
+			KeyValueArray[CurrentArraySize].Key = Key;
+			KeyValueArray[CurrentArraySize].Value = Value;
+
+			CurrentArraySize++;
+			Sort();
+		}
+		/* Check to see if the top element is equal to the current array size and that the array size isn't equal to the Max Size */
+		if (CurrentArraySize == ArraySize) { AddMemory(); } // Will run the function to add memory
+	}
+	/* This function will allow for the removal of an elements in the array */
+	void RemoveKVP(TypeKey Key)
+	{
+		/* Checks to see if the Array is empty */
+		if (!IsEmpty())
+		{
+			int SavedIndex = 0; // Used to store the index of the removed Key
+			bool KeyExists = false; // Boolean to see if the Key exsists
+			/* Runs through all the elements in the array */
+			for (int i = 0; i < CurrentArraySize; i++)
+			{
+				/* This will check to see if the Key is in the array */
+				if (!(KeyValueArray[i].Key < Key) && !(Key < KeyValueArray[i].Key))
+				{
+					SavedIndex = i; // Saves the current index
+					KeyExists = true; // Turns true so we know it exsits
+					break; // break out
+				}
+			}
+			/* If the Key is in the array */
+			if (KeyExists)
+			{
+				KeyValuePair<TypeKey, TypeValue>* KVPArray;
+				KVPArray = new KeyValuePair<TypeKey, TypeValue>[ArraySize]; // Create a new array of KVP
+				/* run throught all the elements in the array */
+				for (int i = 0; i < CurrentArraySize; i++)
+				{
+					/* This will pass over all the elements in the array to the new array
+					   apart from the element at the saved index */
+					if (i != SavedIndex && KeyValueArray != NULL)
+					{
+						KVPArray[i].Key = KeyValueArray[i].Key;
+						KVPArray[i].Value = KeyValueArray[i].Value;
+					}
+				}
+				delete[] KeyValueArray; // Delete all the elements in the all array
+				if (KeyValueArray != NULL)
+				{
+					KeyValueArray = KVPArray; // Sets the pointer to look at the new array
+				}
+				Sort(); // sorts the new array
+				CurrentArraySize--; // Pops the top of array as its no longer an element
+			}
+			else
+			{
+				/* Tells the user that the key doesn't exisit in the map */
+				std::cout << KEY_NOT_FOUND_MESSAGE << std::endl;
+			}
+		}
+	}
+	/* This function is used to reset the array */
+	void Clear()
+	{
+		/* This will delete the array and reset it back to current max size, also creates a new array at the correct size*/
+		delete[] KeyValueArray;
+		KeyValueArray = new KeyValuePair<TypeKey, TypeValue>[ArraySize] {}; // This creates a new array of Keys and values
+		CurrentArraySize = 0;
+	}
+	/* This function will just return the current size of the map */
+	int CurrentSize()
+	{
+		return CurrentArraySize;
+	}
+	/* This function will return the Value asscoited to a Key */
+	TypeValue GetValue(TypeKey Key)
+	{
+		bool KeyInMap = false;
+		/* Runs through all the Keys to see if the passed Key is equal to any in the map */
+		for (int i = 0; i < CurrentArraySize; i++)
+		{
+			/* Check to see if the passed Key is equal to the Key in the array*/
+			if (!(KeyValueArray[i].Key < Key) && !(Key < KeyValueArray[i].Key))
+			{
+				/* Returns what value is associated to the Key */
+				KeyInMap = true;
+				return KeyValueArray[i].Value;
+			}
+		}
+		if (!KeyInMap)
+		{
+			/* Tells the user that the key doesn't exisit in the map */
+			std::cout << KEY_NOT_FOUND_MESSAGE << std::endl;
+		}
+		return NULL;
+	}
+	/*This function will return true or false depending if the key alredy exsists*/
+	bool FindKey(TypeKey Key)
+	{
+		bool KeyInMap = false;
+		/*Runs through all the current Keys in the map*/
+		for (int i = 0; i < CurrentArraySize; i++)
+		{
+			/* Check to see if the passed Key is equal to the Key in the array*/
+			if (!(KeyValueArray[i].Key < Key) && !(Key < KeyValueArray[i].Key))
+			{
+				KeyInMap = true;
+				return true;
+			}
+		}
+		if (!KeyInMap)
+		{
+			/* Tells the user that the key doesn't exisit in the map */
+			std::cout << KEY_NOT_FOUND_MESSAGE << std::endl;
+		}
+		return false;
+	}
 };
 
 
--- OO_Assignment/OO_Assignment/OO_Assignment.cpp
+++ OO_Assignment/OO_Assignment/OO_Assignment.cpp
@@ -1,21 +1,40 @@
 // OO_Assignment.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
 #include <crtdbg.h>
 #include "MapArrayClass.h"
-#include "MapArrayClass.cpp"
 #include <string>
 #include <typeinfo>
 
+struct MyStruct
+{
+
+	float Testfloat;
+public:
+	MyStruct() {}
+	MyStruct(float a) : Testfloat(a) {}
+
+	/*The user has to provide these two function as this will allow them to compare and use the iterator*/
+	bool operator < (MyStruct const& obj)
+	{
+		/*Compares*/
+		return Testfloat < obj.Testfloat;
+	}
+	friend std::ostream& operator<<(std::ostream& os, const MyStruct& obj)
+	{
+		/*Allows for class output to console window*/
+		os << obj.Testfloat;
+		return os;
+	}
+};
 /* This is class which is used for testing*/
 class MyObject
 {
 
 	int TestInt;
 public:
-	MyObject(){}
-	MyObject(int a) : TestInt(a){}
+	MyObject() {}
+	MyObject(int a) : TestInt(a) {}
 
 	/*The user has to provide these two function as this will allow them to compare and use the iterator*/
 	bool operator < (MyObject const& obj)
@@ -50,140 +69,209 @@ void TestInterfaceMethods(T Key[], K Value[])
 	typename MapArray<T, K>::Iterator It;
 	for (It = Map->Begin(); It != Map->End(); ++It)
 	{
-		std::cout << "[" << count << "]" << "Key: " << It->Key << " ," <<  " Value: " << It->Value << std::endl;
+		std::cout << "[" << count << "]" << "Key: " << It->Key << " ," << " Value: " << It->Value << std::endl;
 		count++;
 	}
 	std::cout << "CurrentSize: " << Map->CurrentSize() << std::endl;
-	std::cout << "Trying to find Key: " << Key[8] << std::endl;
-	std::cout << "Is the Key already in the Map: " << Map->FindKey(Key[8]) << std::endl;
+	std::cout << "Trying to find Key: " << Key[18] << std::endl;
+	std::cout << "Is the Key already in the Map: " << Map->FindKey(Key[18]) << std::endl;
 	std::cout << "Getting Value At Key " << Key[12] << " : " << Map->GetValue(Key[12]) << std::endl;
-	std::cout << "Removing element with Key: " << Key[2] << std::endl;
-	Map->RemoveKVP(Key[2]);
+	std::cout << "Removing element with Key: " << Key[12] << std::endl;
+	Map->RemoveKVP(Key[12]);
 	count = 0;
 	for (It = Map->Begin(); It != Map->End(); ++It)
 	{
 		std::cout << "[" << count << "]" << "Key: " << It->Key << " ," << " Value: " << It->Value << std::endl;
 		count++;
 	}
-	std::cout << "CLEARING MAP" << std::endl;
+	std::cout << "Clearing Map" << std::endl;
 	Map->Clear();
+	std::cout << "Is The Map Empty: " << Map->IsEmpty() << std::endl;
 	std::cout << "CurrentSize: " << Map->CurrentSize() << std::endl;
 	count = 0;
-	std::cout << "Is The Map Empty: " << Map->IsEmpty() << std::endl;
+	std::cout << "Adding New Data to Map: " << std::endl;
+	for (int i = 0; i < 26; i++)
+	{
+		Map->AddKVP(Key[i], Value[i]);
+	}
+	std::cout << "CurrentSize: " << Map->CurrentSize() << std::endl;
+	std::cout << "Clearing Map" << std::endl;
 	delete (Map);
 	Map = NULL;
 	std::cout << "///////////////////////////////////////////" << std::endl;
 }
+struct TestData
+{
+	/*Random Datat that will be used when it comes to testing*/
+	int TestI[26] = { 90, 9, 97, 89, 36, 75, 51, 95, 12, 23, 91, 64, 40, 35, 94, 67, 96, 30, 63, 21, 33 , 323 , 321 , 232, 32,3232 };
+	float TestF[26] = { 90.52f, 9.76f, 97.543f, 89.546f, 36.384f, 75.483f, 51.3454f, 97.6575f, 12.23f,
+					  23.876f, 91.23f, 64.92f, 40.3f, 35.6f, 94.2f, 67.8f, 96.98f, 30.23f, 63.2f, 21.2f,
+					  43.21f, 434.52f, 73.76f, 23.57f, 213.53f, 583.91f };
+	std::string TestS[26] = { "Is this a pigeon?" , "Scumbag Steve", "Grumpy Cat", "Darude Sandstorm", "China",
+							"Ceiling Cat", "Trololo", "Chocolate Rain", "Dabbing", "Don't tase me bro", "Nick Young ???",
+							"Its a trap", "Arthur Fist", "Thats what she said", "Doge", "Double Rainbow", "Kermit",
+							"Nyan Cat", "Harambe", "Keyboard Cat", "RickRolling", "TrollFace", "Its just a prank bro", "Fail", "Who's Sam", "Motivational Lizard" };
+	char TestC[26] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O','P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+	bool TestB[26] = { 0, 1 , 0, 1 , 0, 1 , 0, 1,0, 1,0, 1,0, 1,0, 1,0, 1,0, 1,0,1,0,1,0,1 };
+	double TestD[26] = { 84.252 ,92.422,129.853,128.271,123.173,109.645,163.864,123.503,1234.637,123.429,192.046,27.957
+					,96.389,345.344,43.632,567.357,145.675,12.605,78.262,23.397,91.823,92.897,743.782,57.304,65.646,32.302 };
+	MyObject TestUDC[26] = { 44, 82, 8, 46, 27, 27, 32, 6, 20, 31, 18, 22, 4, 81,88, 78, 90, 41, 13, 58,32,57,124,676,324,7 };
+	MyStruct TestUDS[26] = { 1.2884f, 2.5758f,4.0896f,8.8450f,3.1414f,2.0118f,4.9658f,2.7377f,0.4315f,1.1002f,0.2174f,4.3410f,0.2292f,1.7685f,
+						   3.7229f,9.0139f,6.7529f,1.0975f,2.6218f,5.6836f,0.0678f,1.4960f,8.3738f,8.2677f,1.4738f,0.6299f };
 
-/*Random Datat that will be used when it comes to testing*/
-static int TestI[26] = { 90, 9, 97, 89, 36, 75, 51, 95, 12, 23, 91, 64, 40, 35, 94, 67, 96, 30, 63, 21, 33 , 323 , 321 , 232, 32,3232 };
-static float TestF[26] = { 90.52f, 9.76f, 97.543f, 89.546f, 36.384f, 75.483f, 51.3454f, 97.6575f, 12.23f,
-						   23.876f, 91.23f, 64.92f, 40.3f, 35.6f, 94.2f, 67.8f, 96.98f, 30.23f, 63.2f, 21.2f,
-						   43.21, 434.52, 73.76, 23.57, 213.53, 583.91 };
-static std::string TestS[26] = { "Kourtney Ellison" , "Danni Nixon", "Vera Wolf", "Casper Holloway", "Zahraa Mcmanus",
-								 "Amani Foley", "Alba Strickland", "Fatma Rosa", "Lukasz Miles", "Sharna Morrison", "Graeme Conley",
-								 "Osian Case", "Dora Legge", "Israr Chambers", "Hester Workman", "Chandni Stone", "Lea Childs",
-								 "Sienna - Rose North", "Jasleen Phan", "Sheridan Mendoza", "Danny K", "Jack Q", "Hi Li", "Jerry A", "Who's Sam", "Lizard" };
-static char TestC[26] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O','P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-static bool TestB[26] = { 0, 1 , 0, 1 , 0, 1 , 0, 1,0, 1,0, 1,0, 1,0, 1,0, 1,0, 1,0,1,0,1,0,1 };
-static MyObject TestUDC[26] = { 44, 82, 8, 46, 27, 27, 32, 6, 20, 31, 18, 22, 4, 81,88, 78, 90, 41, 13, 58,32,57,124,676,324,7 };
+};
 
 /*Test Cases for Ints*/
-void TestInts()
+void TestInts(TestData& data)
 {
 	std::cout << "ALL INT TEST CASES" << std::endl;
 	std::cout << "" << std::endl;
-	TestInterfaceMethods<int, int>(TestI, TestI);
-	TestInterfaceMethods<int, float>(TestI, TestF);
-	TestInterfaceMethods<int, std::string>(TestI, TestS);
-	TestInterfaceMethods<int, char>(TestI, TestC);
-	TestInterfaceMethods<int, bool>(TestI, TestB);
-	TestInterfaceMethods<int, MyObject>(TestI, TestUDC);
+	TestInterfaceMethods<int, int>(data.TestI, data.TestI);
+	TestInterfaceMethods<int, float>(data.TestI, data.TestF);
+	TestInterfaceMethods<int, std::string>(data.TestI, data.TestS);
+	TestInterfaceMethods<int, char>(data.TestI, data.TestC);
+	TestInterfaceMethods<int, bool>(data.TestI, data.TestB);
+	TestInterfaceMethods<int, double>(data.TestI, data.TestD);
+	TestInterfaceMethods<int, MyObject>(data.TestI, data.TestUDC);
+	TestInterfaceMethods<int, MyStruct>(data.TestI, data.TestUDS);
 }
+
 /*Test Cases for Floats*/
-void TestFloats()
+void TestFloats(TestData& data)
 {
 	std::cout << "ALL FLOAT TEST CASES" << std::endl;
 	std::cout << "" << std::endl;
-	TestInterfaceMethods<float, int>(TestF, TestI);
-	TestInterfaceMethods<float, float>(TestF, TestF);
-	TestInterfaceMethods<float, std::string>(TestF, TestS);
-	TestInterfaceMethods<float, char>(TestF, TestC);
-	TestInterfaceMethods<float, bool>(TestF, TestB);
-	TestInterfaceMethods<float, MyObject>(TestF, TestUDC);
+	TestInterfaceMethods<float, int>(data.TestF, data.TestI);
+	TestInterfaceMethods<float, float>(data.TestF, data.TestF);
+	TestInterfaceMethods<float, std::string>(data.TestF, data.TestS);
+	TestInterfaceMethods<float, char>(data.TestF, data.TestC);
+	TestInterfaceMethods<float, bool>(data.TestF, data.TestB);
+	TestInterfaceMethods<float, double>(data.TestF, data.TestD);
+	TestInterfaceMethods<float, MyObject>(data.TestF, data.TestUDC);
+	TestInterfaceMethods<float, MyStruct>(data.TestF, data.TestUDS);
 }
 /*Test Cases for Strings*/
-void TestStrings()
+void TestStrings(TestData& data)
 {
 	std::cout << "ALL STRING TEST CASES" << std::endl;
 	std::cout << "" << std::endl;
-	TestInterfaceMethods<std::string, int>(TestS, TestI);
-	TestInterfaceMethods<std::string, float>(TestS, TestF);
-	TestInterfaceMethods<std::string, std::string>(TestS, TestS);
-	TestInterfaceMethods<std::string, char>(TestS, TestC);
-	TestInterfaceMethods<std::string, bool>(TestS, TestB);
-	TestInterfaceMethods<std::string, MyObject>(TestS, TestUDC);
+	TestInterfaceMethods<std::string, int>(data.TestS, data.TestI);
+	TestInterfaceMethods<std::string, float>(data.TestS, data.TestF);
+	TestInterfaceMethods<std::string, std::string>(data.TestS, data.TestS);
+	TestInterfaceMethods<std::string, char>(data.TestS, data.TestC);
+	TestInterfaceMethods<std::string, bool>(data.TestS, data.TestB);
+	TestInterfaceMethods<std::string, double>(data.TestS, data.TestD);
+	TestInterfaceMethods<std::string, MyObject>(data.TestS, data.TestUDC);
+	TestInterfaceMethods<std::string, MyStruct>(data.TestS, data.TestUDS);
 }
 /*Test Cases for Chars*/
-void TestChars()
+void TestChars(TestData& data)
 {
 	std::cout << "ALL CHAR TEST CASES" << std::endl;
 	std::cout << "" << std::endl;
-	TestInterfaceMethods<char, int>(TestC, TestI);
-	TestInterfaceMethods<char, float>(TestC, TestF);
-	TestInterfaceMethods<char, std::string>(TestC, TestS);
-	TestInterfaceMethods<char, char>(TestC, TestC);
-	TestInterfaceMethods<char, bool>(TestC, TestB);
-	TestInterfaceMethods<char, MyObject>(TestC, TestUDC);
+	TestInterfaceMethods<char, int>(data.TestC, data.TestI);
+	TestInterfaceMethods<char, float>(data.TestC, data.TestF);
+	TestInterfaceMethods<char, std::string>(data.TestC, data.TestS);
+	TestInterfaceMethods<char, char>(data.TestC, data.TestC);
+	TestInterfaceMethods<char, bool>(data.TestC, data.TestB);
+	TestInterfaceMethods<char, double>(data.TestC, data.TestD);
+	TestInterfaceMethods<char, MyObject>(data.TestC, data.TestUDC);
+	TestInterfaceMethods<char, MyStruct>(data.TestC, data.TestUDS);
 }
-/*Test Cases for Bools*/
-void TestBool()
+/*Test Cases for Doubles*/
+void TestDoubles(TestData& data)
 {
 	std::cout << "ALL BOOL TEST CASES" << std::endl;
 	std::cout << "" << std::endl;
-	TestInterfaceMethods<bool, int>(TestB, TestI);
-	TestInterfaceMethods<bool, float>(TestB, TestF);
-	TestInterfaceMethods<bool, std::string>(TestB, TestS);
-	TestInterfaceMethods<bool, char>(TestB, TestC);
-	TestInterfaceMethods<bool, bool>(TestB, TestB);
-	TestInterfaceMethods<bool, MyObject>(TestB, TestUDC);
+	TestInterfaceMethods<double, int>(data.TestD, data.TestI);
+	TestInterfaceMethods<double, float>(data.TestD, data.TestF);
+	TestInterfaceMethods<double, std::string>(data.TestD, data.TestS);
+	TestInterfaceMethods<double, char>(data.TestD, data.TestC);
+	TestInterfaceMethods<double, bool>(data.TestD, data.TestB);
+	TestInterfaceMethods<double, double>(data.TestD, data.TestD);
+	TestInterfaceMethods<double, MyObject>(data.TestD, data.TestUDC);
+	TestInterfaceMethods<double, MyStruct>(data.TestD, data.TestUDS);
 }
-/*Test Cases for User Defined Classes*/
-void TestUDCs()
+/*Test Cases for User Defined Classes(CLASS)*/
+void TestUDCs(TestData& data)
 {
 	std::cout << "ALL USER DEFINED CLASSES TEST CASES" << std::endl;
 	std::cout << "" << std::endl;
-	TestInterfaceMethods<MyObject, int>(TestUDC, TestI);
-	TestInterfaceMethods<MyObject, float>(TestUDC, TestF);
-	TestInterfaceMethods<MyObject, std::string>(TestUDC, TestS);
-	TestInterfaceMethods<MyObject, char>(TestUDC, TestC);
-	TestInterfaceMethods<MyObject, bool>(TestUDC, TestB);
-	TestInterfaceMethods<MyObject, MyObject>(TestUDC, TestUDC);
+	TestInterfaceMethods<MyObject, int>(data.TestUDC, data.TestI);
+	TestInterfaceMethods<MyObject, float>(data.TestUDC, data.TestF);
+	TestInterfaceMethods<MyObject, std::string>(data.TestUDC, data.TestS);
+	TestInterfaceMethods<MyObject, char>(data.TestUDC, data.TestC);
+	TestInterfaceMethods<MyObject, bool>(data.TestUDC, data.TestB);
+	TestInterfaceMethods<MyObject, double>(data.TestUDC, data.TestD);
+	TestInterfaceMethods<MyObject, MyObject>(data.TestUDC, data.TestUDC);
+	TestInterfaceMethods<MyObject, MyStruct>(data.TestUDC, data.TestUDS);
+}
+/*Test Cases for User Defined Classes(STRUCT)*/
+void TestUDSs(TestData& data)
+{
+	std::cout << "ALL USER DEFINED CLASSES TEST CASES" << std::endl;
+	std::cout << "" << std::endl;
+	TestInterfaceMethods<MyStruct, int>(data.TestUDS, data.TestI);
+	TestInterfaceMethods<MyStruct, float>(data.TestUDS, data.TestF);
+	TestInterfaceMethods<MyStruct, std::string>(data.TestUDS, data.TestS);
+	TestInterfaceMethods<MyStruct, char>(data.TestUDS, data.TestC);
+	TestInterfaceMethods<MyStruct, bool>(data.TestUDS, data.TestB);
+	TestInterfaceMethods<MyStruct, double>(data.TestUDS, data.TestD);
+	TestInterfaceMethods<MyStruct, MyObject>(data.TestUDS, data.TestUDC);
+	TestInterfaceMethods<MyStruct, MyStruct>(data.TestUDS, data.TestUDS);
 }
 
 
 int main()
 {
-
 	/*Will run through the test cases, stop so you can read them and then it clears them for the next lot of test cases*/
-	TestInts();
-	system("pause");
-	system("CLS");
-	TestFloats();
-	system("pause");
-	system("CLS");
-	TestStrings();
-	system("pause");
-	system("CLS");
-	TestChars();
-	system("pause");
-	system("CLS");
-	TestBool();
-	system("pause");
-	system("CLS");
-	TestUDCs();
-	system("pause");
-	system("CLS");
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDOUT);
+	_CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDOUT);
+	{
+		TestData data;
+		TestInts(data);
+	}
 	_CrtDumpMemoryLeaks();
+	system("pause");
+	system("CLS");
+	{
+		TestData data;
+		TestFloats(data);
+	}
+	_CrtDumpMemoryLeaks();
+	system("pause");
+	system("CLS");
+	{
+		TestData data;
+		TestStrings(data);
+	}
+	_CrtDumpMemoryLeaks();
+	system("pause");
+	system("CLS");
+	{
+		TestData data;
+	TestChars(data);
+	}
+	_CrtDumpMemoryLeaks();
+	system("pause");
+	system("CLS");
+	{
+		TestData data;
+		TestDoubles(data);
+	}
+	_CrtDumpMemoryLeaks();
+	system("pause");
+	system("CLS");
+	{
+		TestData data;
+		TestUDCs(data);
+	}
+	_CrtDumpMemoryLeaks();
+	system("pause");
+	system("CLS");
 	return 0;
 }
